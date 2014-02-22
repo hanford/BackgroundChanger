f55d5f4506c64a40dd89b2c9c2cdfe9c0f9ada78
Jack Hanford <jackhanford@gmail.com>
2014-02-21 17:02:21 -0800

    added a ton of new stuff, History for color codes, Remember button, new logic

--- js/main.js
+++ js/main.js
@@ -3,5 +3,33 @@ $(document).ready(function(){
 	$("#bck-grabber").keyup(function(){
 		var background = $("#bck-grabber").val();
 		$(".background").css("background-color", background);
+    valuecheck();
 	});
+
+  $('.rember-color').click(function(){
+    var remember = $('#bck-grabber').val();
+    $('.box').fadeIn();
+    
+    $('<li class="bg">' + remember + '</li>')
+      .css("background-color", remember)
+      .appendTo('.box');
+
+    $(".background").css("background-color", "#999");
+  });
+
+  valuecheck();
 });
+
+
+function valuecheck() {
+  if ($('#bck-grabber').val().length != "4" && $('#bck-grabber').val().length < "7") {
+    $('#bck-grabber').addClass('error');
+  } else {
+    $('#bck-grabber').removeClass('error');
+  };
+  if ($('#bck-grabber').val()[0] != "#" ) {
+    $('.hexi-text').fadeIn();
+  } else {
+    $('.hexi-text').fadeOut();
+  };
+}
