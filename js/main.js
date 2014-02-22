8f4d355a584c0aca9e0a0a2d2eb9cb2962a403b6
Jack Hanford <jackhanford@gmail.com>
2014-02-22 12:33:28 -0800

    Newish design

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ $(document).ready(function(){
     var original = $('#bck-grabber').val();
     $('.box').fadeIn();
 
-    var light = color.lighten(0.2);
+    var light = color.lighten(0.5);
     $('<div class="bg">' + light.hexString() + " Lighter" + '</div>').css("background-color", light.hexString()).appendTo('.box');
 
     $('<div class="bg">' + original + " Original" + '</div>').css("background-color", original).appendTo('.box');
 
-    var dark = color.alpha(0.5).darken(0.5);
+    var dark = color.darken(0.5);
     $('<div class="bg">' + dark.hexString() + " Darker" + '</div>').css("background-color", dark.hexString()).appendTo('.box');
 
 
