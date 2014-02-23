03fe0064eac759b52d1b4e4d35708431f816f635
Jack Hanford <jackhanford@gmail.com>
2014-02-22 17:04:36 -0800

    Broke everything, massive overhaul

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-var fave = new Object();
-
 $(document).ready(function(){
   $.cookie.json = true;
   $('.favorite').append($.cookie("color"));
@@ -11,38 +9,50 @@ $(document).ready(function(){
     valuecheck();
 	});
 
-  $('.fave-color').click(function(){
-    $('.favorite').empty();
-    fave.color = original;
-    var myFavorite = JSON.stringify(fave);
-    console.log(fave);
-    $.cookie("color", original);
-    $('.favorite').append($.cookie("color"));
+  $(document).keypress(function(e) {
+    if(e.keyCode == 13) {
+      e.preventDefault();
+      $('.favorite').empty();
+      $.cookie("color", original);
+      $('.favorite').append($.cookie("color"));
+      treat();
+    }
   });
 
-  $('.rember-color').click(function(){
-    var color = Color($('#bck-grabber').val());
-    $('.box').fadeIn();
-
-    var light = color.lighten(0.5);
-    $('<div class="bg">' + light.hexString() + " Lighter" + '</div>').css("background-color", light.hexString()).appendTo('.box');
-
-    $('<div class="bg">' + original + " Original" + '</div>').css("background-color", original).appendTo('.box');
-
-    var dark = color.darken(0.5);
-    $('<div class="bg">' + dark.hexString() + " Darker" + '</div>').css("background-color", dark.hexString()).appendTo('.box');
-
-    $(".background").css("background-color", "#555");
-  });
-
+  $(".background").css("background-color", "#555");
   valuecheck();
+  favecheck();
 });
+
+function favecheck() {
+  if ($('.favorite').text() === "") {
+    console.log('yes');
+  } else {
+    treat($('.favorite').text());
+  }
+};
+
+function treat() {
+  var color = Color($('#bck-grabber').val());
+  $('.box').fadeIn();
+  var light = color.lighten(0.5);
+  $('<div class="bg">' + light.hexString() + " Lighter" + '</div>').css("background-color", light.hexString()).appendTo('.box');
+  $('<div class="bg">' + original + " Original" + '</div>')
+    .css("background-color", original)
+    .appendTo('.box');
+  var dark = color.darken(0.5);
+  $('<div class="bg">' + dark.hexString() + " Darker" + '</div>')
+    .css("background-color", dark.hexString())
+    .appendTo('.box');
+};
 
 
 function valuecheck() {
   if ($('#bck-grabber').val().length != "4" && $('#bck-grabber').val().length < "7") {
     $('#bck-grabber').addClass('error');
+    $('.title').text("Insert Color code below");
   } else {
+    $('.title').text("Press Enter to save");
     $('#bck-grabber').removeClass('error');
   };
   if ($('#bck-grabber').val()[0] != "#" ) {
